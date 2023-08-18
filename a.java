ae025927d590c7242ca0d61ff64a7f7d728cf16a
Vinilazza <108952051+Vinilazza@users.noreply.github.com>
2023-08-18 09:43:10 -0300

    Update a.java

--- a.java
+++ a.java
@@ -6,8 +6,9 @@ public class Ex4 {
         Scanner sc = new Scanner(System.in);
 
         int conta[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        double saldo[] = { 500, 400, 300, 1200, 3400, 4000, 120, 5, 7, 99 }, vDeposito;
-        int c, option, cDeposito;
+        double saldo[] = { 500, 400, 300, 1200, 3400, 4000, 120, 5, 7, 99 }, vDeposito,sValor,saldoBanco=0;
+        int c, option, cDeposito,cConta;
+        int confirm;
         System.out.println("--- Banco do Vini --- \n"
                 + "Digite qual a sua conta: ");
         c = sc.nextInt();
@@ -19,7 +20,9 @@ public class Ex4 {
                     + "Digite 1 para fazer um deposito \n"
                     + "Digite 2 para efetuar o saque \n"
                     + "Digite 3 para consultar o saldo \n"
-                    + "Digite 4 para encerrar o programa");
+                    + "Digite 4 para consultar o saldo de outra conta \n"
+                    + "Digite 5 para consultar o saldo do banco total \n"
+                    + "Digite 6 para encerrar o programa");
             option = sc.nextInt();
             if (option == 1) {
                 System.out.println("Qual conta voce deseja depositar? ");
@@ -32,23 +35,50 @@ public class Ex4 {
                 if (saldo[c] > vDeposito) {
                         System.out.println("Conta para deposito: "+conta[cDeposito]+"\n"
                         +"Valor do deposito: "+vDeposito+" \n"+
-                        "Confirmar deposito? ");
-                        if(confirm == true) {
-                            
-                        }
-                        saldo[cDeposito] = saldo[cDeposito] + vDeposito;
+                        "Confirmar deposito? (Digite 1)");
+                        confirm = sc.nextInt();
+                        
+                        if(confirm == 1) {
+                                                   saldo[cDeposito] = saldo[cDeposito] + vDeposito;
                         saldo[c] = saldo[c] - vDeposito;
                         System.out.println("O seu novo saldo é " + saldo[c]);
+                        }
+                        else {
+                            System.out.println("Porfavor digite novamente as informações da conta");
+                        }
+ 
     
 
                 }
             } else if (option == 2) {
-
+                System.out.println("Quanto de valor voce deseja sacar?");
+                sValor = sc.nextDouble();
+                if(saldo[c] < sValor) {
+                    System.out.println("Voce não possui esse saldo para sacar. Seu saldo atual é: "+saldo[c]+"\n");
+                }
+                else {
+                    saldo[c] = saldo[c] - sValor;
+                    System.out.println("O seu novo saldo é: "+saldo[c]+"\n");
+                }
             } else if (option == 3) {
-
+                System.out.println("Ola! Seu saldo atual é: "+saldo[c]+"\n");
+            }
+            else if(option == 4) {
+                System.out.println("Qual conta voce deseja consultar? ");
+                cConta = sc.nextInt();
+                System.out.println("A conta "+cConta+" possui "+saldo[cConta]);
+            }
+            else if(option == 5) {
+                for(int i=0;i<saldo.length;i++) {
+                    saldoBanco = saldo[i] + saldoBanco;
+                }
+                System.out.println("O valor total que possuimos no banco é: "+saldoBanco+" \n");
+            }
+            else if(option == 6) {
+                System.out.println("Encerrando sua sessão!");
+                break;
             }
 
-            System.out.println("horas");
 
         }
     }
